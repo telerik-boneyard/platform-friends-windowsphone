62fb477115795a9f1847123c60f72a130e545fd7
Anton <ant.dobrev@gmail.com>
2014-01-25 16:45:58 +0200

    Removed API and Analytics Project keys.

--- ConnectionSettings.cs
+++ ConnectionSettings.cs
@@ -10,12 +10,12 @@ namespace Telerik.Windows.Controls.Cloud.Sample
         /// <summary> 
         /// Input your Backend Services API key below to connect to your own app. 
         /// </summary> 
-        public static string EverliveApiKey = "hNCCe5CHUKCkwvhO";
+        public static string EverliveApiKey = "your-api-key-here";
 
         /// <summary>
         /// The Telerik Analytics project identifier.
         /// </summary>
-        public static string AnalyticsProjectKey = "5f6f78c67d6c4acb92a0b06c30de518e";
+        public static string AnalyticsProjectKey = "your-Analytics-project-key-here";
 
         /// <summary> 
         /// Specified whether to use HTTPS when communicating with Backend Services. 
